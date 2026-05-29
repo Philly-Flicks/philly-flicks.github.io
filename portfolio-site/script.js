676d38c71a18a94f6f0789a43be25bfcf0cf844d
Phaze09 <phillyflickss@gmail.com>
2026-05-29 02:10:22 -0700

    Create script.js

---
+++ portfolio-site/script.js
@@ -0,0 +1,6 @@
+const portfolioBtn = document.getElementById("portfolioBtn");
+const portfolioMenu = document.getElementById("portfolioMenu");
+
+portfolioBtn.addEventListener("click", () => {
+  portfolioMenu.classList.toggle("show");
+});
