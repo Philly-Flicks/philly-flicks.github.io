73abe8394e376dbd4bc5df502f4f6b068758e509
Phaze09 <phillyflickss@gmail.com>
2026-05-30 12:22:51 -0700

    Update photography.js

--- portfolio-site/photography/photography.js
+++ portfolio-site/photography/photography.js
@@ -1,1 +1,34 @@
+const links = document.querySelectorAll('a[href^="#"]');
 
+links.forEach(link => {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    target.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+const animatedItems = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.35
+  }
+);
+
+animatedItems.forEach(item => {
+  observer.observe(item);
+});
